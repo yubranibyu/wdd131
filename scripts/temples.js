c2df07c1852867380cef446b355baa856ceb1cb5
yubrani <yag24001@byui.edu>
2025-03-19 15:58:05 -0300

    templecommit2

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,4 @@
-// Menú hamburguesa
+
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
@@ -6,6 +6,6 @@ menuBtn.addEventListener("click", () => {
     menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
 });
 
-// Actualizar año y última modificación en el footer
+
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
